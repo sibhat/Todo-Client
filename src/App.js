03f35a8891f3898c449c3352436ff52d54c3a865
sibhat <sibhat.temelso@gmail.com>
2019-04-01 19:09:13 -0700

    implementing nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import {withRouter} from "react-router-dom";
+import {withStyles, CssBaseline} from "@material-ui/core";
+import {connect} from "react-redux";
+import compose from "recompose/compose";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+// import TransitionGroup from "react-transition-group/TransitionGroup";
+import Nav from "./feature/nav/container";
+import List from "./feature/list/container/List"; // ES6
+
+class App extends React.Component {
+
+    render() {
+        let {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <CssBaseline/> {/* reset css for all devices */}
+                <Nav/>
+                <List/>
+            </div>
+        );
+    }
 }
 
-export default App;
+const DispatchStateToProps = state => ({});
+
+export default compose(
+    withRouter,
+    connect(
+        DispatchStateToProps,
+    ),
+    withStyles(styles)
+)(App);
